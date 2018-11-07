560db9ef27c1d0820d0148391313e182c83fc89e
william <willianmoreira@gmail.com>
2018-11-07 08:48:45 -0200

    initial files

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <p>Novo curso:
+      <input type="text" v-model="novo" @keyup.enter="incluir">
+        <button @click="incluir">Inserir</button>
+    </p>
+    <ul>
+      <li v-for="curso in cursos" :key="curso.id"> {{ curso.nome }} </li>    
+    </ul>
+  </div>
+</template>
+
+<script>
+import CursoItem from './components/CursoItem';
+ export default {
+ name: 'app',
+ components: {
+      CursoItem
+ },
+ data() {
+    return {
+          cursos: [
+            { nome: 'ADS'},
+            { nome: 'Banco de Dados'}
+          ]
+        }
+      },
+
+  methods: {
+    incluir(event) {
+      if(this.novo.trim().length == 0) {
+        alert('O nome do curso deve ser preenchido!')
+      }
+      else {
+        this.cursos.push( { nome: this.novo } );
+      }
+    },
+    excluir(indice) {
+      this.cursos.splice(indice, 1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
